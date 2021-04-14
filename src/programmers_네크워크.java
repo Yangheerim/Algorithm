c540d70fc70cd874f87e3a82ec58037ebe83e07c
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-04-14 22:45:57 +0900

    programmers-네트워크

--- src/programmers_네크워크.java
+++ src/programmers_네크워크.java
@@ -24,4 +24,35 @@ public class programmers_네크워크 {
         }
     }
 
+    // BFS로 풀이
+//    public int solution(int n, int[][] computers) {
+//        int answer = 0;
+//        visited = new boolean[computers.length];
+//
+//        for (int i = 0; i < computers.length; i++) {
+//            if (!visited[i]) {
+//                answer++;
+//                BFS(i, computers);
+//            }
+//        }
+//        return answer;
+//    }
+//
+//    public static void BFS(int i, int[][] arr) {
+//        Queue<Integer> queue = new LinkedList<>();
+//        queue.add(i);
+//
+//        while (!queue.isEmpty()) {
+//            int cur = queue.poll();
+//            visited[cur] = true;
+//
+//            for (int j = 0; j < arr.length; j++) {
+//                if (!visited[j] && arr[cur][j] == 1) {
+//                    queue.add(j);
+//                }
+//            }
+//        }
+//    }
+
+
 }
