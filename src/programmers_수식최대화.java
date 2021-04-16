c3bb51a662d8c7dffe4004796e48832ced2372b6
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-04-17 01:05:00 +0900

    programmers 수식최대화

--- src/programmers_수식최대화.java
+++ src/programmers_수식최대화.java
@@ -1,16 +1,22 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class programmers_수식최대화 {
+    static ArrayList<String> priority_list;
+    static HashSet<Character> set;
+    static int n;
 
     public static void main(String[] args) {
         solution("100-200*300-500+20");
+        solution("50*6-3*2");
+        solution("2-990-5+2");
     }
 
     public static long solution(String expression) {
         long answer = 0;
 
-        ArrayList<String> arr = new ArrayList<>();
-        boolean[] flag = new boolean[3];
+        ArrayList<String> expression_array = new ArrayList<>();
+        set = new HashSet<>();
 
         // 수식 나눠서 ArrayList에 저장
         String num = "";
@@ -18,42 +24,84 @@ public class programmers_수식최대화 {
             char ch = expression.charAt(i);
 
             if(ch == '+' || ch =='-' || ch == '*') {
-                arr.add(num);
-                arr.add(""+ch);
+                expression_array.add(num);
+                expression_array.add(""+ch);
                 num = "";
 
-                switch (ch){
-                    case '+': flag[0] = true; break;
-                    case '-': flag[1] = true; break;
-                    case '*': flag[2] = true; break;
-                }
+                set.add(ch);
 
                 continue;
             }
             num += ch;
         }
-        arr.add(num); // 마지막 숫자
+        expression_array.add(num); // 마지막 숫자
 
-//        for (String s : arr) {
-//            System.out.println(s);
-//        }
+        n = set.size();
+        getPriority();
 
+        for (String s : priority_list) {
 
+            // initialize
+            ArrayList<String> arr = new ArrayList<>();
+            for(String ss : expression_array){
+                arr.add(ss);
+            }
 
+            for(int i=0; i<n; i++){ //n은 연산자의 개수
+                char ope = s.charAt(i);
 
-        char[] operator = new char[3]; // 0->1->2순위
-
-
+                for(int j=1; j<arr.size();j=j+2){
+                    if(arr.get(j).charAt(0) == ope){
+                        long a = Long.parseLong(arr.get(j - 1));
+                        long b = Long.parseLong(arr.get(j + 1));
+                        long result = 0;
+                        switch (ope){
+                            case '+': result=a+b; break;
+                            case '-': result=a-b; break;
+                            case '*': result=a*b; break;
+                        }
+                        arr.remove(j - 1);
+                        arr.remove(j-1);
+                        arr.remove(j-1);
+                        arr.add(j - 1, String.valueOf(result));
+                        j=-1;
+                    }
+                }
+            }
+            answer = Math.max(answer, Math.abs(Long.parseLong(arr.get(0))));
+        }
+        System.out.println(answer);
         return answer;
     }
 
-    public void getPriority(boolean[] operation){
-        ArrayList<String> prio_list = new ArrayList<>();
-
-        for()
-
+    public static void getPriority(){
+        priority_list = new ArrayList<>();
+        char[] operations = new char[n];
+        boolean[] visited = new boolean[n];
+        int i=0;
+        for (Character c:set) {
+            operations[i++] = c;
+        }
+        dfs("", operations, visited);
     }
-    public void dfs(){
+
+    // 조합 아니고 순열으로 풀어야함. 경우의 수니까!
+    public static void dfs(String result, char[] operations, boolean[] visited){
+        if(result.length()==n){
+            priority_list.add(result);
+            return;
+        }
+
+        for(int i=0; i<n; i++){
+
+            if(!visited[i]){
+                visited[i] = true;
+                result += operations[i];
+                dfs(result, operations, visited);
+                result = result.substring(0, result.length() - 1);
+                visited[i] = false;
+            }
+        }
 
     }
 
