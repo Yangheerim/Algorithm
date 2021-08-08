2dc3471e36b1b5b35c922986afbd1472d0b4a47b
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-08-08 14:30:55 +0900

    baekjoon 2206

---
+++ src/boj2206_벽부수고이동하기.java
@@ -0,0 +1,110 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+반례)
+
+8 8
+01000100
+01010100
+01010100
+01010100
+01010100
+01010100
+01010100
+00010100
+
++ BFS는 먼저 도착했을 경우 무조건 더 빠르다 !!
+
+ */
+
+
+public class boj2206_벽부수고이동하기 {
+    static class Loc{
+        int i;
+        int j;
+        int cnt;
+        boolean destroyed;
+
+        public Loc(int i, int j, int cnt, boolean d) {
+            this.i = i;
+            this.j = j;
+            this.cnt = cnt;
+            this.destroyed = d;
+        }
+    }
+
+    // visited를 3중 배열로!
+    // visited[n][m][0]은 벽을 한번도 안부순 애들이 탐색한 경우, visited[n][m][1]은 벽을 한번 부수고 탐색한 경우
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+
+        char[][] map = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = input.charAt(j);
+            }
+        }
+
+
+        Queue<Loc> q = new LinkedList<>();
+        q.add(new Loc(0, 0, 1, false));
+
+        int[] mi = {0, 0, -1, 1};
+        int[] mj = {-1, 1, 0, 0};
+
+        boolean[][][] visited = new boolean[n][m][2];
+
+        while (!q.isEmpty()) {
+            Loc now = q.poll();
+
+            if (now.i == n - 1 && now.j == m - 1) {
+                System.out.println(now.cnt);
+                return;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int ni = now.i + mi[d];
+                int nj = now.j + mj[d];
+
+                if(ni<0 || ni>=n || nj<0 || nj>=m) continue;
+
+                int next_cnt = now.cnt+1;
+
+                if(map[ni][nj]=='0'){ // 벽이 아니면
+                    if(!now.destroyed && !visited[ni][nj][0]) { // 부신 벽이 여태까지 없었으면
+                        q.add(new Loc(ni, nj, next_cnt, false));
+                        visited[ni][nj][0] = true;
+                    }else if(now.destroyed && !visited[ni][nj][1]){ // 벽을 한번 부신 적이 있으면
+                        q.add(new Loc(ni, nj, next_cnt, true));
+                        visited[ni][nj][1] = true;
+                    }
+
+                }else if(map[ni][nj]=='1'){ // 벽이면
+
+                    if(!now.destroyed){ // 한번도 벽을 부순적이 없다면 부순다~
+                        q.add(new Loc(ni, nj, next_cnt, true));
+                        visited[ni][nj][1] = true;
+                    }
+                    // 한번 부순 적이 있다면 또 부수고 갈 수 없기 때문에 pass
+                }
+            }
+
+        }
+
+        System.out.println(-1);
+    }
+}
