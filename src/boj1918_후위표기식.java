51dfbfd0613a658892dfdb66abdff005c66b1a41
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-08-15 14:57:45 +0900

    baekjoon 1918

---
+++ src/boj1918_후위표기식.java
@@ -0,0 +1,63 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+//https://mygumi.tistory.com/181
+public class boj1918_후위표기식 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String str = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char now = str.charAt(i);
+
+            switch (now){
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    while (!stack.isEmpty() && priority(stack.peek()) >= priority(now)) {
+                        sb.append(stack.pop());
+                    }
+                    stack.add(now);
+                    break;
+                case '(':
+                    stack.add(now);
+                    break;
+                case ')':
+                    while(!stack.isEmpty() && stack.peek() != '('){
+                        sb.append(stack.pop());
+                    }
+                    stack.pop();
+                    break;
+                default:
+                    sb.append(now);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    public static int priority(char operator){
+
+        if(operator=='(' || operator==')'){
+            return 0;
+        } else if (operator == '+' || operator == '-') {
+            return 1;
+        } else if (operator == '*' || operator == '/') {
+            return 2;
+        }
+        return -1;
+    }
+
+}
