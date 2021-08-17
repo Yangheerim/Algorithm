e007fbddc71899768e4cae16aece1145aa618858
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-08-17 13:34:20 +0900

    2021 kakao intership

---
+++ src/programmers_숫자문자열과영단어.java
@@ -0,0 +1,41 @@
+package programmers;
+
+import java.util.HashMap;
+
+public class programmers_숫자문자열과영단어 {
+
+    public int solution(String s) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("zero", 0);
+        map.put("one", 1);
+        map.put("two", 2);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
+        map.put("six", 6);
+        map.put("seven", 7);
+        map.put("eight", 8);
+        map.put("nine", 9);
+
+        String result = "";
+        String tmp = "";
+        for (int i = 0; i < s.length(); i++) {
+            char now = s.charAt(i);
+            if(Character.isDigit(now)){
+                result += now;
+                tmp = "";
+            }else{
+                tmp += now;
+                if (map.get(tmp) != null) {
+                    result += map.get(tmp);
+                    tmp = "";
+                }
+            }
+        }
+
+
+        return Integer.parseInt(result);
+    }
+
+}
