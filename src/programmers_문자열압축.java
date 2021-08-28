c6b40f9326ec31d4cddadefac7896dc72c8bd257
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-08-29 00:12:52 +0900

    programmers 문자열압축

--- src/programmers_문자열압축.java
+++ src/programmers_문자열압축.java
@@ -2,28 +2,37 @@ package programmers;
 
 public class programmers_문자열압축 {
 
-    public int solution(String s) {
+    public static void main(String[] args) {
+        solution("abcabcabcabcdededededede");
+
+    }
+
+    public static int solution(String s) {
         int answer = s.length();
 
-
-        for (int i = 1; i < s.length(); i++) {
-            if(s.length() % 2 != 0) continue;
+        for (int i = 1; i <= s.length()/2; i++) {
+            System.out.println("i="+i);
 
             int idx = 0;
             String prev = "";
             String result = "";
             int cnt = 1;
 
-            while(idx+i < s.length()){
-                String now = s.substring(idx, idx + i);
+            while(idx < s.length()){
+                String now;
+                if(idx+i<s.length()) {
+                    now = s.substring(idx, idx + i);
+                }else{
+                    now = s.substring(idx, s.length());
+                }
 
                 if(now.equals(prev)) { // 전꺼랑 같으면
                     cnt ++;
                 }else { // 전꺼랑 다르면
                     if(cnt>1){
-                        result += (cnt + now);
+                        result += (cnt + prev);
                     }else{
-                        result += now;
+                        result += prev;
                     }
                     cnt = 1;
                 }
@@ -32,11 +41,16 @@ public class programmers_문자열압축 {
                 idx += i;
 
             }
-            System.out.println(result);
+
+            if(cnt>1){
+                result += (cnt + prev);
+            }else{
+                result += prev;
+            }
+
             answer = Math.min(answer, result.length());
 
         }
-
 
         return answer;
     }
