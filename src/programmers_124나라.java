0f8de8cf9c3322149c366afbc762b13c04803397
Yang, heerim <37904738+Yangheerim@users.noreply.github.com>
2021-09-26 23:13:33 +0900

    programmers 124나라

--- src/programmers_124나라.java
+++ src/programmers_124나라.java
@@ -1,23 +1,23 @@
 package programmers;
 
-// https://cnwlcjf.tistory.com/64
+// https://velog.io/@hyeon930/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4-124-%EB%82%98%EB%9D%BC%EC%9D%98-%EC%88%AB%EC%9E%90-Java
 // 각자리수 계산을 위해 나머지를 구하는 과정에서 나머지가 0일 경우 나누어지는 수 n에서 1을 빼줘야한다.
 
 public class programmers_124나라 {
     public String solution(int n) {
         String answer = "";
-        int namugy = 0;
+        String[] numbers = {"4", "1", "2"};
+
 
         while (n > 0) {
-            namugy = n % 3;
+            int namugy = n % 3;
             n = n / 3;
 
             if (namugy == 0) {
                 n -= 1;     //???
-                namugy = 4;
             }
 
-            answer = namugy + answer;
+            answer = numbers[namugy] + answer;
         }
 
         return answer;
